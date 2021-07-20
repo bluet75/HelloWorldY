8e56e0ecc0123647c0520a9168b7cf625555e6fe
bluet75 <bluet75@naver.com>
2021-07-20 23:32:37 +0900

    master branch changes a code

--- HelloWorldY/src/HelloWorldY/HelloWorld.java
+++ HelloWorldY/src/HelloWorldY/HelloWorld.java
@@ -5,7 +5,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HelloWorldY");
-        System.out.println( "World's End Dancehall" );    		
+        System.out.println( "World's End Dancehall" );
+        System.out.println( "Earth's End Dancehall" );          
 	}
 
 }
