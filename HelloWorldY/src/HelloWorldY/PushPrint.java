e216c74843e21c4cadab82402f4617b7a2d80471
bluet75 <bluet75@naver.com>
2021-07-23 07:53:26 +0900

    added PushPrint @HelloWorldY first

---
+++ HelloWorldY/src/HelloWorldY/PushPrint.java
@@ -0,0 +1,10 @@
+package HelloWorldY;
+
+public class PushPrint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloWorldY Add Java Class File Push");
+	}
+
+}
